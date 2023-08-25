86abfe49187742e5702cc2db11732549ff03d883
Aman0246 <amankashyap0246@gmail.com>
2023-08-25 12:30:30 +0530

    Backend Done

--- server.js
+++ server.js
@@ -2,19 +2,13 @@ const express = require('express');
 const app = express();
 require('dotenv').config()
 
-
-// Other configurations and middleware setup...
-
-// Include the upload and download routes
-// const uploadRoute = require('./routes/upload');
-// const downloadRoute = require('./routes/download');
 const { router } = require('./Routes');
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 app.use('/',router);
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
